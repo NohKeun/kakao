fe57b3c76f315c3865fe258f40c57890dafd8635
nkpark <nkpark1017@gmail.com>
2024-11-10 23:53:47 +0900

    멀티모듈로 + 추상메서드 패턴 적용

--- api/deposit/src/main/java/com/kakao/online/transfer/TransferController.java
+++ api/deposit/src/main/java/com/kakao/online/transfer/TransferController.java
@@ -2,12 +2,14 @@ package com.kakao.online.transfer;
 
 import com.kakao.online.transfer.iomodel.TransferInput;
 import com.kakao.online.transfer.service.Transfer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/transfer")
 @RestController
 public class TransferController {
@@ -16,10 +18,10 @@ public class TransferController {
     private Transfer transfer;
 
     @PostMapping("/send")
-    public String insertAppVersion(@RequestBody TransferInput input) throws Exception {
-
+    public String send(@RequestBody TransferInput input) throws Exception {
+        log.debug("aaa");
         transfer.send();
 
-        return null;
+        return "abc";
     }
 }
