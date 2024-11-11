15bc40cb2e2c7092acc75b7794f1c1b9f3ae9352
nkpark <nkpark1017@gmail.com>
2024-11-11 21:21:58 +0900

    정상 구동

--- onlinecore/src/main/java/com/kakao/Application.java
+++ onlinecore/src/main/java/com/kakao/Application.java
@@ -1,14 +1,13 @@
-package com.kakao.online;
+package com.kakao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello Real");
 
 		SpringApplication.run(Application.class, args);
 	}
