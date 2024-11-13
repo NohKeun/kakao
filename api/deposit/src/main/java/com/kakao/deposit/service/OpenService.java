0daccf2ccb1b2d76a9f66c86d1d39fb18110840c
nkpark <nkpark1017@gmail.com>
2024-11-13 11:21:34 +0900

    불필요 서비스는 삭제

--- api/deposit/src/main/java/com/kakao/deposit/service/OpenService.java
+++ api/deposit/src/main/java/com/kakao/deposit/service/OpenService.java
@@ -1,9 +1,5 @@
 package com.kakao.deposit.service;
 
-import com.kakao.domain.inf.common.NotificationInf;
-import com.kakao.domain.inf.deposit.DepositInf;
-import com.kakao.domain.inf.factory.CommonFactory;
-import com.kakao.domain.inf.factory.DepositFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,30 +9,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OpenService {
 
-    //의존성주입 매서드
-    private final CommonFactory commonFactory;
-    private final DepositFactory depositFactory;
-
-    //구현체 호출 인터페이스
-    private NotificationInf notificationInf;
-    private DepositInf depositInf;
-
-    //Dao Repository
-
     public void execute() {
 
-        this.notificationInf = commonFactory.injectionNotificationInf("kakaoTalk");
-        this.notificationInf.sendNotification();
-
-        //1.injection(의존성주입)
-
-        //2.validate
-        //-출금계좌 검증
-        //-
-
-        //2.고객정보 검증
-
     }
-
-
 }
