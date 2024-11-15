d6881f14ba8a3fe6dd4790447f7b70f3e462ab45
nkpark <nkpark1017@gmail.com>
2024-11-15 16:14:00 +0900

    문서 추가

--- onlinecore/src/main/java/com/kakao/core/repository/BaseRepositoryImpl.java
+++ onlinecore/src/main/java/com/kakao/core/repository/BaseRepositoryImpl.java
@@ -9,19 +9,37 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public class BaseRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements BaseRepository<T,ID> {
 
+    EntityManager entityManager;
+
     public BaseRepositoryImpl(Class<T> domainClass, EntityManager em) {
         super(domainClass, em);
+        entityManager = em;
     }
 
     @Override
-    public <S extends T> S insert(S entity) throws Exception
-    {
-        return save(entity);
+    public <S extends T> S insert(S entity) throws Exception {
+        try {
+            //persist로 중복으로 insert하는 경오 오류처리
+            entityManager.persist(entity);
+        } catch (Exception e) {
+            throw new Exception();
+        }
+        return entity;
     }
 
 
     @Override
     public <S extends T> S update(S entity) throws Exception {
-        return save(entity);
+
+        //업데이트 전에 pk조회 후 값이 존재하지 않으면 오류
+        Object pk = entityManager.getEntityManagerFactory().getPersistenceUnitUtil().getIdentifier(entity);
+        T exists = entityManager.find(this.getDomainClass(), pk);
+
+        if (exists == null) {
+            throw new Exception();
+        }
+
+        entity = entityManager.merge(entity);
+        return entity;
     }
 }
